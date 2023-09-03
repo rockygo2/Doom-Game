da25b427928a10bb38d16270c3c6bce9dd434a2b
Maurice Ibrahim <113593028+rockygo2@users.noreply.github.com>
2023-09-03 08:39:25 +0200

    Initial commit

---
+++ src/snake/logic/GameLogic.scala
@@ -0,0 +1,56 @@
+package snake.logic
+
+import engine.random.{RandomGenerator, ScalaRandomGen}
+import snake.logic.GameLogic._
+
+/** To implement Snake, complete the ``TODOs`` below.
+ *
+ * If you need additional files,
+ * please also put them in the ``snake`` package.
+ */
+class GameLogic(val random: RandomGenerator,
+                val gridDims : Dimensions) {
+
+  def gameOver: Boolean = false
+
+  // TODO implement me
+  def step(): Unit = ()
+
+  // TODO implement me
+  def setReverse(r: Boolean): Unit = ()
+
+  // TODO implement me
+  def changeDir(d: Direction): Unit = ()
+
+  // TODO implement me
+  def getCellType(p : Point): CellType = Empty()
+
+}
+
+/** GameLogic companion object */
+object GameLogic {
+
+  val FramesPerSecond: Int = 5 // change this to increase/decrease speed of game
+
+  val DrawSizeFactor = 1.0 // increase this to make the game bigger (for high-res screens)
+  // or decrease to make game smaller
+
+  // These are the dimensions used when playing the game.
+  // When testing the game, other dimensions are passed to
+  // the constructor of GameLogic.
+  //
+  // DO NOT USE the variable DefaultGridDims in your code!
+  //
+  // Doing so will cause tests which have different dimensions to FAIL!
+  //
+  // In your code only use gridDims.width and gridDims.height
+  // do NOT use DefaultGridDims.width and DefaultGridDims.height
+  val DefaultGridDims
+    : Dimensions =
+    Dimensions(width = 25, height = 25)  // you can adjust these values to play on a different sized board
+
+
+
+}
+
+
