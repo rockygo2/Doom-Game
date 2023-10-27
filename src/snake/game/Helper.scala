591d951effcf3811a7c32fde508d3d615b84e81f
rockygo2 <rockygo2@outlook.com>
2023-10-27 17:15:21 +0200

    working here and cool

--- src/snake/game/Helper.scala
+++ src/snake/game/Helper.scala
@@ -1,7 +1,7 @@
 package snake.game
 
 object Helper {
-  def normalizeAngle(angle: Int): Int = {
+  def NormalizeAngle(angle: Int): Int = {
     var normalizedAngle = angle % 360
     if (normalizedAngle < 0) {
       normalizedAngle += 360
@@ -9,6 +9,6 @@ object Helper {
     normalizedAngle
   }
   def getTanInverse(x : Double, y : Double): Double = {
-    normalizeAngle(math.toDegrees(math.atan2(y, x)).toInt)
+    NormalizeAngle(math.toDegrees(math.atan2(y, x)).toInt)
   }
 }
